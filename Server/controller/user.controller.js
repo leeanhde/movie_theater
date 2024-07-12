be02b0dcc116fbfbc18bdc2119271c6ecd62ee49
leeanhde <114909280+leeanhde@users.noreply.github.com>
2024-07-12 17:33:42 +0700

    update cinemaroom

--- Server/controller/user.controller.js
+++ Server/controller/user.controller.js
@@ -1,4 +1,5 @@
-const { User } = require("../model/index");
+const db = require('../model/index');
+const User = require('../model/user.model');
 
 const getCustomerById = async (req, res, next) => {
   try {
@@ -15,4 +16,82 @@ const getCustomerById = async (req, res, next) => {
   }
 };
 
-module.exports = { getCustomer };
+// View user profile
+const viewProfile = async (req, res, next) => {
+  try {
+    const userId = req.user.id; // Assuming the user's ID is available in the request
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found", data: null });
+    }
+
+    res.status(200).json({ user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Edit user profile
+const editProfile = async (req, res, next) => {
+  try {
+    await User.findByIdAndUpdate(
+      req.user.id,
+      {
+        $set: {
+          email: req.body.email,
+          username: req.body.username,
+          fullName: req.body.fullName,
+          dob: req.body.dob,
+          gender: req.body.gender,
+          phoneNumber: req.body.phoneNumber,
+          address: req.body.address,
+          image: req.body.image
+        }
+      },
+      { new: true, runValidators: true }
+    );
+
+    const updatedUser = await User.findById(req.user.id);
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found", data: null });
+    }
+
+    res.status(200).json({ user: updatedUser });
+  } catch (error) {
+    next(error);
+  }
+};
+
+async function list(req, res, next) {
+  try {
+    const list = await User.find();
+    const newList = list.map(r => ({
+      _id: r._id,
+      username: r.username,
+      email: r.email,
+      fullName: r.fullName,
+      phoneNumber: r.phoneNumber,
+      address: r.address
+    }));
+    res.status(200).json(newList);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function deleteUserById(req, res, next) {
+  try {
+      const userId = req.params.id;
+      await User.findByIdAndDelete(userId);
+      res.status(204).json({ message: " deleted successfully" });
+  } catch (error) {
+      next(error);
+  }
+}
+
+const UserController = { 
+  getCustomerById, viewProfile, editProfile, list, deleteUserById
+ };
+module.exports = UserController;
--- Server/routes/movie.route.js
+++ Server/routes/movie.route.js
@@ -10,5 +10,9 @@ movieRouter.get('/movielist', MovieController.listMovies);
 movieRouter.get('/:id', MovieController.getMovieById);
 movieRouter.put('/update/:id', MovieController.editMovie);
 movieRouter.delete('/delete/:id', MovieController.deleteMovie);
+movieRouter.get('/now-showing', MovieController.nowShowingMovies);
+movieRouter.get('/coming-soon', MovieController.comingSoonMovies);
+movieRouter.get('/detail/:id', MovieController.getMovieDetail);
+movieRouter.get('/searchbyname/:movieName', MovieController.getMovieByName);
 
 module.exports = movieRouter;
